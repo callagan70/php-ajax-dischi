caf219f10e45cd606e34eb496665cec9fe4065e4
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-05-09 18:31:46 +0100

    prima milestone fatta

--- data/database.php
+++ data/database.php
@@ -42,6 +42,34 @@
             'year' => 2002,
             'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
             'genre' => 'Rock'
+        ],
+        [
+            'title' => 'Ten\'s Summoner\'s Tales',
+            'author' => 'Sting',
+            'year' => 1993,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+            'genre' => 'Pop'
+        ],
+        [
+            'title' => 'Steve Gadd band',
+            'author' => 'Steve Gadd Band',
+            'year' => 2018,
+            'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+            'genre' => 'Jazz'
+        ],
+        [
+            'title' => 'Brave new World',
+            'author' => 'Iron Maiden',
+            'year' => 2000,
+            'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
+            'genre' => 'Metal'
+        ],
+        [
+            'title' => 'One more car, one more rider',
+            'author' => 'Eric Clapton',
+            'year' => 2002,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+            'genre' => 'Rock'
         ]
     ];
 
--- index.php
+++ index.php
@@ -20,37 +20,35 @@ include  __DIR__ . './data/database.php'
 <!-- Stampare a schermo una decina di dischi musicali (vedi screenshot).
 Utilizzare:
 Html, CSS, VueJS, axios, PHP
+
 Prima Milestone:
 Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+
 Seconda Milestone:
 Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà, attraverso una chiamata api, i dischi a php e li stamperà attraverso vue.
+
 Bonus:
 Attraverso un’altra chiamata api, filtrare gli album per genere -->
 
 <div id="app">
-    
-<?php
 
-foreach ($database as $disk){
-    echo '<div class="display">';
-        echo '<div class="cards">';
-            // echo '<img src=" ' . $poster . ' " /> ' ;
-            echo "<h2>" . $disk['title'] . "</h2>";
-            echo "<p>" . $disk['author'] .  "</p>";
-        echo "</div>";
-    echo "</div>";
-};
-
-?>
-
-
+    <div id="container">
+                <div class="display">
+                    <?php
+                        foreach ($database as $disk){
+                                echo '<div class="cards">';
+                                    echo '<img src=" ' . $disk['poster'] . ' " > ' ;
+                                    echo '<h2>' . $disk['title'] . "</h2>";
+                                    echo "<p>" . $disk['author'] .  "</p>";
+                                    echo "<p>" . $disk['year'] .  "</p>";
+                                echo "</div>";
+                        };
+                    ?>
+                </div>
+    </div>
 
 
 </div>
-
-
-
-
 <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script src="./file/main.js"></script>
