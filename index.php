1d2a203a2cb416eb033b42165a3a46df25c0509f
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-05-09 15:59:41 +0100

    Si parte ! Con errori....

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+include  __DIR__ 'database.php';
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./file/style.css">
+    <title>PhP Spoty</title>
+</head>
+<body>
+
+<!-- Stampare a schermo una decina di dischi musicali (vedi screenshot).
+Utilizzare:
+Html, CSS, VueJS, axios, PHP
+Prima Milestone:
+Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+Seconda Milestone:
+Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà, attraverso una chiamata api, i dischi a php e li stamperà attraverso vue.
+Bonus:
+Attraverso un’altra chiamata api, filtrare gli album per genere -->
+
+
+
+
+
+<script src="./file/main.js></script>
+<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+</body>
+</html>
